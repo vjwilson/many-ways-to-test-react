90922753bb9a06d24661f6af0a3e1c78c2639c3b
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-10-16 21:21:31 -0400

    Add Seat component to ticket-buying functionality

--- ex6-mocha-enzyme-es6-react-router/src/routes.js
+++ ex6-mocha-enzyme-es6-react-router/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './App/App';  //eslint-disable-line import/no-named-as-default
 import HomePage from './Home/HomePage';
-import BuyTicketsPage from './BuyTickets/BuyTicketsPage';
+import BuyTicketsPage from './Tickets/BuyTicketsPage';
 import initialCastMembers from './castMemberData.js';
 import initialUser from './initialUserData';
 

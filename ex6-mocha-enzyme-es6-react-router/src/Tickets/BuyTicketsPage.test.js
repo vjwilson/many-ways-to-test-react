bbe25da5bb9dee746ab130f66943e6ba62a92c9e
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-10-22 10:16:41 -0400

    Add integration tests to Buy Tickets page

--- ex6-mocha-enzyme-es6-react-router/src/Tickets/BuyTicketsPage.test.js
+++ ex6-mocha-enzyme-es6-react-router/src/Tickets/BuyTicketsPage.test.js
@@ -94,7 +94,7 @@ describe('BuyTicketsPage component', function() {
 
       const buyTicketsPage = mount(<BuyTicketsPage route={routeObject} />);
 
-      // automatically set the state of our
+      // automatically set the state of selected seats and the package to buy
       const pageInstance = buyTicketsPage.instance();
 
       pageInstance.updateSeatStatus('C1');
@@ -110,6 +110,81 @@ describe('BuyTicketsPage component', function() {
       expect(seatArray[2][0].sold).to.be.true;
       expect(seatArray[2][1].sold).to.be.true;
       expect(ticketPackage.seats).to.have.lengthOf(0);
+    });
+
+    it('should handle selecting a seat from its seating chart component (the right way, using the component API)', function() {
+      routeObject.initialSeatData = JSON.parse(JSON.stringify(mockSeatData));
+
+      const buyTicketsPage = mount(<BuyTicketsPage route={routeObject} />);
+
+
+      // find the elements to act upon
+      const seatC1 = buyTicketsPage.findWhere(n => n.prop('sold') === false && n.text() === 'C1');
+      const seatC2 = buyTicketsPage.findWhere(n => n.prop('sold') === false && n.text() == 'C2');
+
+      // act
+      seatC1.simulate('click');
+      seatC2.simulate('click');
+
+      const seatArray = buyTicketsPage.state('seatData');
+      const ticketPackage = buyTicketsPage.state('ticketPackage');
+
+      // assert
+      expect(seatArray[2][0].selected).to.be.true;
+      expect(seatArray[2][1].selected).to.be.true;
+      expect(ticketPackage.seats).to.have.lengthOf(2);
+    });
+
+    it('should handle deselecting a seat from its seating chart component (the right way, using the component API)', function() {
+      routeObject.initialSeatData = JSON.parse(JSON.stringify(mockSeatData));
+
+      const buyTicketsPage = mount(<BuyTicketsPage route={routeObject} />);
+
+      // find the elements to act upon
+      const seatC1 = buyTicketsPage.findWhere(n => n.prop('sold') === false && n.text() === 'C1');
+      const seatC2 = buyTicketsPage.findWhere(n => n.prop('sold') === false && n.text() == 'C2');
+
+      // act
+      // first clicks on each element should select them
+      seatC1.simulate('click');
+      seatC2.simulate('click');
+
+      // second click on one of the elements should deselect them
+      seatC1.simulate('click');
+
+      const seatArray = buyTicketsPage.state('seatData');
+      const ticketPackage = buyTicketsPage.state('ticketPackage');
+
+      expect(seatArray[2][0].selected).to.be.false;
+      expect(seatArray[2][1].selected).to.be.true;
+      expect(ticketPackage.seats).to.have.lengthOf(1);
+    });
+
+    it('should handle selecting seats and checking out (the right way, using the component API)', function() {
+      routeObject.initialSeatData = JSON.parse(JSON.stringify(mockSeatData));
+
+      const buyTicketsPage = mount(<BuyTicketsPage route={routeObject} />);
+
+
+      // find the elements to act upon
+      const seatC1 = buyTicketsPage.findWhere(n => n.prop('sold') === false && n.text() === 'C1');
+      const seatC2 = buyTicketsPage.findWhere(n => n.prop('sold') === false && n.text() == 'C2');
+      const ticketForm = buyTicketsPage.find('.ticket-form');
+
+      // act
+      seatC2.simulate('click');
+      seatC1.simulate('click');
+      ticketForm.simulate('submit');
+
+      const seatArray = buyTicketsPage.state('seatData');
+      const ticketPackage = buyTicketsPage.state('ticketPackage');
+
+      // assert
+      expect(seatArray[2][0].selected).to.equal(false, 'first seat should no longer be selected');
+      expect(seatArray[2][1].selected).to.equal(false, 'second seat should no longer be selected');
+      expect(seatArray[2][0].sold).to.equal(true, 'first seat should now be sold');
+      expect(seatArray[2][1].sold).to.equal(true, 'second seat should now be sold');
+      expect(ticketPackage.seats).to.have.lengthOf(0, 'there should be no more seats ready to checkout');
     });
   });
 });
